8c03d7409022073218c3758ef4e004d71be769f3
morphiezy <myardan.aqiella22@gmail.com>
2022-02-18 17:50:58 +0700

    add javascript validation

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const form = document.getElementById('regist-form')
+// const firstName = document.getElementById('firstname')
+// const lastName = document.getElementById('lastname')
+// const email = document.getElementById('email')
+// const nim =  document.getElementById('nim')
+
+
+function allFormFill(){
+
+    const dataPendaftaran = {
+        firstName : document.getElementById('firstname').value,
+        lastName: document.getElementById('lastname').value,
+        email:document.getElementById('email').value,
+        nim : document.getElementById('nim').value,
+        asalSekolah: document.getElementById('sma').value,
+        gender:document.querySelector('input[name="gender"]').value
+    }
+
+    const result = [];
+
+    for (const data in dataPendaftaran) {
+        result.push(dataPendaftaran[data])
+    }
+
+    return result.every(value => value !== "")
+}
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    allFormFill()
+})
